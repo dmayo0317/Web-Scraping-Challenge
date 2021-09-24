17ca05918c67c96396be24bbe8218934f29e099f
dmayo0317 <86526443+dmayo0317@users.noreply.github.com>
2021-09-23 23:13:48 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/scrape_mars")
+
+
+@app.route("/")
+def home():
+
+    destination_data = mongo.db.collection.find_one()
+    return render_template("index.html", scrape_mars=destination_data)
+
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_facts = scrape_mars.scrape()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_facts, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
